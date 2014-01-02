36dbc1f2912a3c76d3ba052bc1a27521068c2453
Arti Sinani <artisinani@gmail.com>
2014-01-02 22:18:03 +0000

    generator

--- lib/generators/icofont_generator.rb
+++ lib/generators/icofont_generator.rb
@@ -1,26 +1,25 @@
 require 'tmpdir'
 require 'fileutils'
+require_relative '../icofont'
 
-module Icofont
-	class IcofontGenerator < Rails::Generators::Base
-		include FileUtils::Verbose
+class IcofontGenerator < Rails::Generators::Base
+	include FileUtils::Verbose
 
-		GITHUB_REPO = 'https://github.com/lugolabs/icofont-glyphs'
+	GITHUB_REPO = 'https://github.com/lugolabs/icofont-glyphs'
 
-		desc "Generates the SVG fonts file based on glyphs on #{Paths.glyphs_path}"
-		def create_svg
-			create_file Paths.glyphs_path
-		end
+	desc "Generates the SVG fonts file based on glyphs on #{Icofont::Paths.glyphs_path}"
+	def create_svg
+		create_file Icofont::Paths.glyphs_path
+	end
 
-		desc 'Copies the SVG glyphs necessary to generate the icofonts'
-		def copy_glyphs
-			svg_root = Paths.svg_root
-			unless Dir.exists?(svg_root)
-				tmp_dir = Dir.mktmpdir
-				git clone: "#{GITHUB_REPO} #{tmp_dir}"
-				mv File.join(tmp_dir, Paths.svg_folder), svg_root
-				rm_rf tmp_dir
-			end
+	desc 'Copies the SVG glyphs necessary to generate the icofonts'
+	def copy_glyphs
+		svg_root = Icofont::Paths.svg_root
+		unless Dir.exists?(svg_root)
+			tmp_dir = Dir.mktmpdir
+			git clone: "#{GITHUB_REPO} #{tmp_dir}"
+			mv File.join(tmp_dir, Icofont::Paths.svg_folder), svg_root
+			rm_rf tmp_dir
 		end
 	end
 end
--- lib/icofont/version.rb
+++ lib/icofont/version.rb
@@ -1,3 +1,3 @@
 module Icofont
-  VERSION = "0.0.1"
+  VERSION = "0.0.2"
 end
